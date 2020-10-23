4a7e8b74b7bb711777b052b6e28a10b1ca86bc27
SofiaAlves <alveslaura.sofia@gmail.com>
2020-10-23 13:48:32 +0200

    reindentation du fichier

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
- Marmottemenu.printHeader();
- Marmottemenu.printMenu();
- Marmottemenu.printMenu2();
+        Marmottemenu.printHeader();
+        Marmottemenu.printMenu();
+        Marmottemenu.printMenu2();
     }
 }
