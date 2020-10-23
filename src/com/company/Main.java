9237a3922c8b2032b919a29515ca9458d66155e7
SofiaAlves <sofia-laura.amador-alves@edu.itescia.fr>
2020-10-23 13:42:47 +0200

    Creation du menu

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+ Marmottemenu.printHeader();
+ Marmottemenu.printMenu();
+ Marmottemenu.printMenu2();
+    }
+}
